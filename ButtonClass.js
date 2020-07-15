8058ff03a1f0bcdc4f2e9ca282276d4369146513
Kunakbaev <53218382+Kunakbaev@users.noreply.github.com>
2020-07-15 21:15:02 +0300

    Update ButtonClass.js

--- ButtonClass.js
+++ ButtonClass.js
@@ -1,1 +1,19 @@
-
+function button(w, h, x, y, c) {
+  this.w = w;
+  this.h = h;
+  this.x = x;
+  this.y = y;
+  
+  this.color = color(c, c, c);
+  this.draw = function() {
+     fill(this.color);
+     rect(this.x, this.y, this.w, this.h);
+  }
+  this.isClicked = function() {
+    if (mouseX > this.x && mouseX < this.x + this.w
+       && mouseY > this.y & mouseY < this.h + this.y) {
+      return true;
+    }
+    return false;
+  }
+}
